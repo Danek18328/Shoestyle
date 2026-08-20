88bf8045196f6193b34c02339a5e6e182739d1ba
Daniel <jdanielrodri573@gmail.com>
2026-08-19 23:03:05 -0500

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,17 +1,23 @@
 <?php
 
-$servername = getenv('DB_HOST') ?: 'localhost';
-$username   = getenv('DB_USER') ?: 'root';
-$password   = getenv('DB_PASSWORD') ?: '';
-$dbname     = getenv('DB_NAME') ?: 'shoestyle';
-$port       = getenv('DB_PORT') ?: 3306;
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASSWORD');
+$dbname     = getenv('DB_NAME');
+$port       = (int)getenv('DB_PORT');
 
-$conexion = new mysqli(
+$conexion = mysqli_init();
+
+mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+
+$conexion->real_connect(
     $servername,
     $username,
     $password,
     $dbname,
-    (int)$port
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
 if ($conexion->connect_error) {
